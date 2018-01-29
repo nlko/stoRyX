1be06deb93d3e3428442d665ab1dc5fffad74f29
nlko <nospam1@thomasson.fr>
2018-01-29 18:07:03 +0100

    Add test for List

---
+++ src/list.spec.ts
@@ -0,0 +1,101 @@
+import { List } from './list'
+import { hot, cold } from 'jasmine-marbles';
+
+interface T { a: string }
+
+describe('List Tests', () => {
+  it('As a USER, I can create a List', () => {
+    expect(new List<number>([-1])).toEqual(jasmine.any(List))
+  })
+
+  it('As a USER, I must set a default valuet to a List', () => {
+    const s = new List<number>([-1])
+    expect(s.data$).toBeObservable(cold('a', { a: [-1] }));
+  })
+
+  it('As a USER, I can add a value to the list.', () => {
+    const defaultValue = [0]
+
+    // Create the state
+    const s = new List<number>(defaultValue)
+
+    s.add(1)
+
+    expect(s.data$).toBeObservable(cold('a', { a: [0, 1] }));
+
+  })
+
+  it('As a USER, I can remove a value from a list of object.', () => {
+    const defaultValue: T[] = []
+
+    let i = 100
+
+    // Create the state
+    const s = new List<T>(defaultValue, 'ID', (): string => '' + i++)
+
+    s.add({ a: 'nico1' })
+
+    expect(s.data$).toBeObservable(cold('a', { a: [{ a: 'nico1', ID: '100' }] }));
+
+    s.add({ a: 'nico2' }).subscribe(
+      id => {
+
+        expect(s.data$).toBeObservable(cold('a', { a: [{ a: 'nico1', ID: '100' }, { a: 'nico2', ID: id }] }));
+
+        s.remove(id)
+
+        expect(s.data$).toBeObservable(cold('a', { a: [{ a: 'nico1', ID: '100' }] }));
+
+      })
+
+  })
+
+  it('As a USER, I can remove a value from a list of string.', () => {
+    const defaultValue = ['hello']
+
+    // Create the state
+    const s = new List<string>(defaultValue)
+
+    s.add('nico')
+
+    expect(s.data$).toBeObservable(cold('a', { a: ['hello', 'nico'] }));
+
+    s.remove(0)
+
+    expect(s.data$).toBeObservable(cold('a', { a: ['nico'] }));
+  })
+
+  it('As a USER, I can search an object using its id.', () => {
+    const defaultValue: T[] = []
+
+    let i = 100
+
+    // Create the state
+    const s = new List<T>(defaultValue, 'ID', (): string => '' + i++)
+
+    s.add({ a: 'nico1' })
+
+    expect(s.data$).toBeObservable(cold('a', { a: [{ a: 'nico1', ID: '100' }] }));
+
+    s.add({ a: 'nico2' }).subscribe(id => {
+      expect(s.data$).toBeObservable(cold('a', { a: [{ a: 'nico1', ID: '100' }, { a: 'nico2', ID: id }] }));
+
+      expect(s.find(id)).toBeObservable(cold('a', { a: { a: 'nico2', ID: '101' } }));
+    })
+  })
+
+  it('As a USER, I can search a in from a list of string.', () => {
+    const defaultValue = ['hello']
+
+    // Create the state
+    const s = new List<string>(defaultValue)
+
+    s.add('nico')
+
+    expect(s.data$).toBeObservable(cold('a', { a: ['hello', 'nico'] }));
+
+    expect(s.find(0)).toBeObservable(cold('a', { a: 'hello' }));
+    expect(s.find(1)).toBeObservable(cold('a', { a: 'nico' }));
+
+  })
+})
--- src/list.ts
+++ src/list.ts
@@ -1,26 +1,49 @@
 import { State } from './state'
+import { Informer } from './informer'
 import { uuidv4 } from './uuidv4'
+import { Observable } from 'rxjs/src/Observable'
+import 'rxjs/src/add/operator/map'
 
 export class List<T, ID=string> extends State<T[]>{
+
+  get length$(): Observable<number> {
+    return this.data$.map(x => x.length)
+  }
+
   constructor(initialContent: T[] = [], protected idField = "_id", private idGenerator = uuidv4) {
     super(initialContent)
   }
 
-  remove(id: string) {
-    this.update(state => state.filter(c => c[this.idField] != id))
+  remove(id: string | number): void {
+    if (typeof id == 'number')
+      this.update(state => { state.splice(id, 1); return state })
+    else
+      this.update(state => state.filter(c => c[this.idField] != id))
   }
 
-  add(element: T) {
-    if (!element[this.idField]) {
+  add(element: T): Observable<number | string> {
+    const id = new Informer<number | string>()
+
+    if (typeof element == 'object' && element[this.idField] == undefined) {
       element[this.idField] = this.idGenerator()
     }
 
-    this.update(state => [...state, element])
+    this.update(state => {
+      if (typeof element == 'object' && element[this.idField] != undefined) {
+        id.inform(element[this.idField])
+      } else {
+        id.inform(state.length)
+      }
+      return [...state, element]
+    })
 
-    return element[this.idField]
+    return id.obs$
   }
 
-  find(id: string): T {
-    return null
+  find(id: string | number): Observable<T> {
+    return this.data$.map((arr: T[]): T => {
+      if (typeof id == 'number') return arr[id]
+      else return arr.find(c => c[this.idField] == id)
+    })
   }
 }
