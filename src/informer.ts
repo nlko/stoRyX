1e4c8f9cbde565b9fd57e614d0dcda568f8a35ce
nlko <nospam1@thomasson.fr>
2018-01-29 18:06:12 +0100

    Add Informer

---
+++ src/informer.ts
@@ -0,0 +1,13 @@
+import { AsyncSubject } from 'rxjs/src/AsyncSubject'
+import { Observable } from 'rxjs/src/Observable'
+
+export class Informer<T> {
+  private sub$ = new AsyncSubject<T>()
+  public inform(val: T): void {
+    this.sub$.next(val)
+    this.sub$.complete()
+  }
+  public get obs$(): Observable<T> {
+    return this.sub$.asObservable()
+  }
+}
