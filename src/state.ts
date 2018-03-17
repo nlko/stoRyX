c3b59dc13be7f68f41d7f530833d26a44145eed7
nlko <nospam1@thomasson.fr>
2018-03-17 16:45:13 +0100

    Simplify obs$ implementation in State

--- src/state.ts
+++ src/state.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/scan'
 
 export type StateUpdateFn<S> = (s: S) => S
 export class State<S> implements Holder<S>{
-  private data$: Observable<S>
+  readonly obs$: Observable<S>
   private currentState$: BehaviorSubject<S>
 
   private _updater$: Subject<StateUpdateFn<S>>
@@ -17,16 +17,12 @@ export class State<S> implements Holder<S>{
   constructor(initialValue: S) {
     this._updater$ = new Subject<StateUpdateFn<S>>()
     this.currentState$ = new BehaviorSubject<S>(initialValue)
-    this.data$ = this.currentState$.asObservable()
+    this.obs$ = this.currentState$.asObservable()
     const dispatcher = (state: S, op: StateUpdateFn<S>) => op(state)
     this._updater$.scan(dispatcher, initialValue).subscribe(this.currentState$)
   }
 
   public update(fn: StateUpdateFn<S>): void {
     this._updater$.next((state: S) => fn(state))
-  }
-
-  public get obs$(): Observable<S> {
-    return this.data$
   }
 }
