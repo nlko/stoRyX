d0ace76d07146eb597d013aad63c0dbeaa2daa51
nlko <nospam1@thomasson.fr>
2018-05-26 16:57:30 +0200

    Add map doc

--- src/map.ts
+++ src/map.ts
@@ -6,10 +6,31 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/filter'
 import 'rxjs/add/operator/distinctUntilChanged'
 
+/** Type of data describing a [[Map]]. */
 export type MapState = any
+/** Type of data stored in a [[Map]] associated to a key. */
 export type MapData = any
+/** Type of data for keys that reference data in a [[Map]]. */
 export type MapName = string
+
+/** Type of message to use to modify a value in the [[Map]] with the set$s
+ * subject attribute.
+ * @param name - key of the data to update in the [[Map]];
+ * @param data - the new data value.
+ */
 export type MapSetMessage = { name: MapName, data: MapData }
+
+/** A map object that contained data element indexed using a string.
+ *
+ * It is possible to retrieve the number of element stored in the map using
+ * the [[length]] attribute.
+ *
+ * It is possible to subscribe to the change of an element of the Map using the
+ * [[get$]] and [[getOr$]] methods.
+ *
+ * The map inherit from the @see State object and can be subscribed to using
+ * the obs$ observable (this allows to detect changes).
+ */
 export class Map extends State<MapState> implements ILength {
 
   /** return an Observable of the length */
@@ -17,9 +38,26 @@ export class Map extends State<MapState> implements ILength {
     return this.obs$.map(x => Object.keys(x).length)
   }
 
+  /** Subject that can be used to set a new element value by sending a
+   * [[MapSetMessage]] into.
+   */
   set$s = new Subject<MapSetMessage>()
-  delete$s = new Subject<string>()
 
+  /** Subject than can be used to delete an element by sending the key of the
+   * element to delete.
+   */
+  delete$s = new Subject<MapName>()
+
+  /**
+   * @param initialValue - The initial value of map
+   * Example:
+   * ```
+   * // construct an empty map
+   * const m1 = new Map()
+   * // construct a map with some elements inside
+   * const m1 = new Map({config:{lang:"fr"}, users:["Nicolas", "Fred"]})
+   * ```
+   */
   constructor(initialValue: MapState = {}) {
     super(initialValue)
 
@@ -34,25 +72,52 @@ export class Map extends State<MapState> implements ILength {
     }).subscribe(this.updater$s)
   }
 
-  set(name: MapName, data: any): void {
+  /** Set an element value
+   * @param name - key of the data to update in the [[Map]];
+   * @param data - the new element value.
+   */
+  set(name: MapName, data: MapData): void {
     this.set$s.next({ name, data })
   }
 
-  getOr$ = (defValue: any, name: MapName): MapData => this.obs$
+  /** retrieve an element with default value.
+   *
+   * This function tries to return the value associated to the name key or
+   * otherwise return the default value defValue.
+   * @param defValue - the default value to be returned
+   * @param name - key of the data to retrieve
+   * @return an observable on the data found.
+   */
+  getOr$ = (defValue: MapData, name: MapName): MapData => this.obs$
     .map(map => map[name] !== undefined ? map[name] : defValue)
     .distinctUntilChanged()
 
+  /** retrieve an element.
+   * This function tries to return the value associated to the name key
+   * @param name - key of the data to retrieve
+   **/
   get$ = (name: MapName): MapData => this.obs$
     .filter(map => map[name] !== undefined)
     .map(map => map[name])
     .distinctUntilChanged()
 
-  isSet$ = (name: MapName): Observable<Boolean> => this.obs$.map(map => map[name] !== undefined).distinctUntilChanged()
+  /** check if an element exist.
+   * @param name - key of the data to check
+   * @return an boolean observable indicating whetever the element exists or not.
+   */
+  isSet$ = (name: MapName): Observable<Boolean> => this.obs$
+    .map(map => map[name] !== undefined)
+    .distinctUntilChanged()
 
+  /** remove an element from the map.
+   * @param name - key of the data todelete
+   */
   delete(name: MapName): void {
     this.delete$s.next(name)
   }
 
+  /** remove all element from the map.
+   */
   flush(): void {
     this.updater$s.next(_ => ({}))
   }
